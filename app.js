b4658e1803c6771b31277f6e39b6c03921ee3897
Watonnum <adamcorktail@gmail.com>
2024-04-29 21:34:26 +0700

    gotcha

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+const country = "Thailand";
+const continent = "Asia";
+console.log(`number is ${2}`); //number is ...
+console.log(`result is ${1 + 3}`);
+console.log(`I live in ${country}`);
+console.log(`I live in ${country}, ${continent}`);
+console.log(`I live in ${country + ", " + continent}`);
+console.log(`I live in ${"country, continent"}`);
